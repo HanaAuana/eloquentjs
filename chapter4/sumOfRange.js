26871b2478b0015d1537b6148ecfd1ff45c712eb
Michael Lim <hanaauana@gmail.com>
2014-09-23 19:36:23 -0700

    EloquentJS solution + added deflt. step for reverse

--- chapter4/sumOfRange.js
+++ chapter4/sumOfRange.js
@@ -1,7 +1,21 @@
-var range = function(start, end){
+var range = function(start, end, step){
 	var nums = [];
-	for (var i = start; i <= end; i++) {
-		nums.push(i);
+
+	if(step === undefined){
+		step = 1;
+	}
+	if(end < start){
+		step = -1;
+	}
+	if( step > 0){
+		for (var i = start; i <= end; i+=step) {
+			nums.push(i);
+		}
+	}
+	else{
+		for (var j = start; j >= end; j+=step) {
+			nums.push(j);
+		}
 	}
 	return nums;
 };
@@ -14,3 +28,5 @@ var sum = function(nums){
 	return sum;
 };
 console.log(sum(range(1,10)));
+
+console.log(range(5,2, -1));
