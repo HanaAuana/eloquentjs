17c5467b0e095fcc0b95f37c0b889ab7dc291bee
Michael Lim <hanaauana@gmail.com>
2014-09-27 20:09:06 -0700

    EloquentJS solution

--- chapter4/deepCompare.js
+++ chapter4/deepCompare.js
@@ -1,31 +1,30 @@
 var deepEqual = function(obj1, obj2){
 
-	if(typeof obj1 !== "object" || typeof obj2 !== "object" || typeof obj1 === null || typeof obj2 === null){
-		return obj1 === obj2;
+	if( obj1 === obj2){
+		return true;
 	}
-	else if(Object.keys(obj1).length !== Object.keys(obj2).length){
+	if(typeof obj1 !== "object" || typeof obj2 !== "object" ||
+		typeof obj1 === null || typeof obj2 === null){
 		return false;
 	}
-	else{
-		for( var o in obj1){
-			if( !(o in obj2) ){
-				return false;
-			}
-			else{
-				if(typeof obj1[o] !== "object"){
-					if(obj1[o] !== obj2[o]){
-						return false;
-					}
-				}
-				else{
-					if(deepEqual(obj1[o], obj2[o]) === false){
-						return false;
-					}
-				}
-			}
+	
+	if(Object.keys(obj1).length !== Object.keys(obj2).length){
+		return false;
+	}
+
+	var numPropsIn1 = 0;
+	var numPropsIn2 = 0;
+	for( var p1 in obj1){
+		numPropsIn1++;
+	}
+
+	for( var p2 in obj2){
+		numPropsIn2++;
+		if( !(p2 in obj1) || !deepEqual(obj1[p2], obj2[p2])){
+			return false;
 		}
 	}
-	return true;
+	return numPropsIn1 === numPropsIn2;
 };
 
 var obj = {
