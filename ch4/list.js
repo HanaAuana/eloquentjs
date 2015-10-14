4ddb1377ebc9e75978bba4f2f6443e908d1395db
Michael Lim <michaellim@surveymonkey.com>
2015-10-13 18:42:09 -0700

    4.3: Refactor nth, arrayToList, and listToArray

--- ch4/list.js
+++ ch4/list.js
@@ -7,51 +7,37 @@ var prepend = function(newVal, list){
 };
 
 var nth = function(target, list){
-  var currNode = list;
-  for(var i = 0; i < target; i++){
-    currNode = currNode.rest;
+  if(!list){
+   return undefined;
   }
-  return currNode || undefined;
-};
-
-var recursiveNth = function(target, depth, list){
-  if(depth === target){
-    return list;
+  else if(target === 0){
+    return list.value;
   }
   else{
-    return recursiveNth(target, depth-1, list.rest);
+    return nth(target-1, list.rest);
   }
 };
 
 var arrayToList = function(input){
-  var output = {};
-  var nextVal = input.shift();
-  var currNode = output;
-  while(nextVal){
-    currNode.value = nextVal;
-    currNode.rest = {};
-    currNode = currNode.rest;
-    nextVal = input.shift();
+  var output = null;
+  for(var i = input.length-1; i >= 0; i--){
+    output = {
+      value:input[i],
+      rest: output
+    };
   }
-  currNode.rest = null;
   return output;
 };
 
 var listToArray = function(input){
   var output = [];
-  var nextVal = input.value;
-  var currNode = input;
-  while(nextVal){
-    output.push(nextVal);
-    currNode = currNode.rest;
-    if(!currNode){
-      break;
-    }
-    nextVal = currNode.value;
+  for(var n = input; n; n = n.rest){
+    output.push(n.value);
   }
   return output;
 };
 
-console.log(arrayToList([1, 2, 3]));
-var testList = arrayToList([1, 2, 3]);
-console.log(listToArray(testList));
+console.log(arrayToList([10, 20]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(1, arrayToList([10, 20, 30])));
