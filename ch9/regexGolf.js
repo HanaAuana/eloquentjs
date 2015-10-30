210361f5fc1af412eb9ce13544e75c97a055cc62
Michael Lim <michaellim@surveymonkey.com>
2015-10-30 06:43:29 -0700

    9.1: Complete exercise

---
+++ ch9/regexGolf.js
@@ -0,0 +1,52 @@
+var counter = 1;
+function verify(regexp, yes, no) {
+  // Ignore unfinished exercises
+  if (regexp.source == "..."){
+    console.log(counter+" Skipped ");
+    counter++;
+    return;
+  }
+  yes.forEach(function(s) {
+    if (!regexp.test(s)){
+      console.log(counter+" Failure to match '" + s + "'");
+      return;
+    }
+  });
+  no.forEach(function(s) {
+    if (regexp.test(s)){
+      console.log(counter+" Unexpected match for '" + s + "'");
+      return;
+    }
+  });
+  console.log(counter+" All passed");
+  counter++;
+}
+
+// 1
+verify(/ca[r|t]/,
+       ["my car", "bad cats", "car", "cat"],
+       ["camper", "high art"]);
+// 2
+verify(/pr?op/,
+       ["pop culture", "mad props", "pop", "props"],
+       ["plop"]);
+// 3
+verify(/ferr(et|y|ari)/,
+       ["ferret", "ferry", "ferrari"],
+       ["ferrum", "transfer A"]);
+// 4
+verify(/\b\w+ious\b/,
+       ["how delicious", "spacious room"],
+       ["ruinous", "consciousness"]);
+// 5
+verify(/\s[.,:;]/,
+       ["bad punctuation ."],
+       ["escape the dot"]);
+// 6
+verify(/\b\w{7,}\b/,
+       ["hottentottententen"],
+       ["no", "hotten totten tenten"]);
+// 7
+verify(/\b([a-d]|[g-z])+\b/,
+       ["red platypus", "wobbling nest"],
+       ["earth bed", "learning ape"]);
