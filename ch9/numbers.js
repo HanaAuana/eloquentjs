084b4812e7eba3fa68999e999f5fa701669381ab
Michael Lim <michaellim@surveymonkey.com>
2015-11-04 15:15:01 -1000

    9.3: Simplify number matching regex

--- ch9/numbers.js
+++ ch9/numbers.js
@@ -1,6 +1,7 @@
 // Fill in this regular expression.
-var number = /^[+-]?(\d+|\d*\.\d+|\d+\.\d*)([eE][+-]?\d+)?$/;
-
+//var number = /^[+-]?(\d+|\d*\.\d+|\d+\.\d*)([eE][+-]?\d+)?$/;
+//Optimized
+var number = /^[+-]?(\d+(\.\d*)?|\.\d+)([eE][+-]?\d+)?$/;
 // Tests:
 ["1", "-1", "+15", "1.55", ".5", "5.", "1.3e2", "1E-4",
  "1e+12"].forEach(function(s) {
